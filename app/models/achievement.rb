3c858b6ac6a9d18fca2afa4ffed91e1c2b65b20c
Georgy Shabunin <georgy.shabunin@gmail.com>
2016-04-23 16:45:36 +0200

    specs for achievements model validations

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,6 +2,17 @@ class Achievement < ActiveRecord::Base
 	belongs_to :user
 
   validates :title, presence: true
+  validate :unique_title_for_one_user
+  # validates :title, uniqueness: true
 
 	enum privacy: [:public_access, :private_access, :friend_access]
+
+  private
+
+  def unique_title_for_one_user
+    existing_achievement = Achievement.find_by(title: title)
+    if existing_achievement && existing_achievement.user == user
+      errors.add(:title, 'This title already exists')
+    end
+  end
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'requires title' do
+      achievement = Achievement.new(title: '')
+      achievement.valid?
+      expect(achievement.errors[:title]).to include("can't be blank")
+    end
+
+    it 'requires title to be unique for one user' do
+      user = FactoryGirl.create(:user)
+      some_achievement = FactoryGirl.create(:public_achievement, title: 'My Achievement', user: user)
+      new_achievement = Achievement.new(title: 'My Achievement', user: user)
+      expect(new_achievement.valid?).to be_falsy
+    end
+
+    it 'allows to have same titles for different users' do
+      user1 = FactoryGirl.create(:user)
+      user2 = FactoryGirl.create(:user)
+      user1_achievement = FactoryGirl.create(:public_achievement, title: 'My Achievement', user: user1)
+      user2_achievement = Achievement.new(title: 'My Achievement', user: user2 )
+      expect(user2_achievement.valid?).to be_truthy
+    end
+  end
 end
